a89e192f55c288779fc28576eeece4910ecf245f
himalay12345 <47078695+himalay12345@users.noreply.github.com>
2020-05-14 09:23:18 +0530

    Layout added and express-ejs-layout installed

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const router = require('./routes/index');
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 app.use('/', router);
 app.set('view engine','ejs');
 app.set('views','./views');
+
 
 
 app.listen(port,function(err)
