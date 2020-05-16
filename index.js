9a819206ff338b938aaeef13c6c70784ca409636
himalay12345 <47078695+himalay12345@users.noreply.github.com>
2020-05-16 12:16:20 +0530

    Fetching data from Signup page manually

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const port = 8000;
 const app = express();
 const router = require('./routes/index');
 const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose')
+const db = require('./config/mongoose');
+const cookieParser = require('cookie-parser');
 
+app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(expressLayouts);
 app.use(express.static('./assets'));
 app.set('layout extractStyles',true);
